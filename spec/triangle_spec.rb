5a815fbee0d09d85242dad829d83b6f901f0d18a
Panela Leung <leungpanela@gmail.com>
2019-10-03 03:27:10 +0000

    Automatically backed up by Learn

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -42,7 +42,6 @@ describe 'Triangle' do
   end
 
   it 'knows that triangles with no size are illegal' do
-    binding.pry
     expect{Triangle.new(0, 0, 0).kind}.to raise_error(Triangle::TriangleError)
   end
 
