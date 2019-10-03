29df9a413d15c6a98bb50d6aca7361afbf55c498
Panela Leung <leungpanela@gmail.com>
2019-10-03 03:58:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,7 +29,8 @@ class Triangle
 
   def kind
     if valid?
-      @side_a != 0 && @side_a == @side_b && @side_a == @side_c && @side_b == @side_c && @side_a != 0
+      # @side_a != 0 &&
+      @side_a == @side_b && @side_a == @side_c && @side_b == @side_c && @side_a != 0
       :equilateral
     elsif
       @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
