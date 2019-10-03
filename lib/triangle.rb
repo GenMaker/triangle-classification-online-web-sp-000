702e99941ea19b7a30a7f5527f16868026f521f8
Panela Leung <leungpanela@gmail.com>
2019-10-03 03:31:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,9 +14,9 @@ class Triangle
     if
       @side_a == 0 && @side_b == 0 && @side_c == 0
       raise TriangleError
-    # elsif
-    #   @side_a || @side_b || @side_c < 0
-    #   raise TriangleError
+    elsif
+      @side_a < 0 || @side_b < 0 || @side_c < 0
+      raise TriangleError
     elsif
       @side_a != 0 && @side_a == @side_b && @side_a == @side_c && @side_b == @side_c && @side_a != 0
       :equilateral
